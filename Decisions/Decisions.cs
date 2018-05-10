8eb1353e9508622a47f23d04312fc116bfa939d2
unknown <mhurley@hurley-consulting.io>
2018-05-10 09:52:50 -0400

    Initial

---
+++ Decisions/Decisions.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Decisions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Mike's Big Giveaway");
+            Console.Write("Choose a Door: 1, 2, or 3: ");
+            string userValue = Console.ReadLine();
+
+            if (userValue == "1")
+            {
+                string message = "You won a new car!";
+                Console.WriteLine(message);
+            }
+
+            Console.ReadLine();
+
+        
+        }
+    }
+}
